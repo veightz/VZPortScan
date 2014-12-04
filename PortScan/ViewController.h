b82d95d3d11ac2dfb4078d0373a9c00049d2910c
Veight Zhou <veightz@gmail.com>
2014-12-04 14:51:51 +0800

    Some..

--- PortScan/ViewController.h
+++ PortScan/ViewController.h
@@ -9,7 +9,14 @@
 #import <Cocoa/Cocoa.h>
 
 @interface ViewController : NSViewController
+@property (weak, nonatomic) IBOutlet NSTextField *hostTextField;
+@property (weak, nonatomic) IBOutlet NSTextField *fromPort;
+@property (weak, nonatomic) IBOutlet NSTextField *toPort;
 
+@property IBOutlet NSTextView *resultScrollView;
+
+@property (weak, nonatomic) IBOutlet NSButton *scanButton;
+- (IBAction)scanHandle:(NSButton *)sender;
 
 @end
 
--- PortScan/ViewController.m
+++ PortScan/ViewController.m
@@ -7,12 +7,19 @@
 //
 
 #import "ViewController.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [_resultScrollView setEditable:NO];
     // Do any additional setup after loading the view.
 }
 
@@ -22,4 +29,58 @@
     // Update the view, if already loaded.
 }
 
+- (IBAction)scanHandle:(NSButton *)sender {
+    int fromPort = [[_fromPort stringValue] intValue];
+    int toPort = [[_toPort stringValue] intValue];
+    [self scrollView:_resultScrollView addLine:@"====== START ======"];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        for (int i = fromPort; i < toPort; i++) {
+            if ([self scanHost:_hostTextField.stringValue withPort:i]) {
+                printf("Open %d\n", i);
+                NSString *str = [NSString stringWithFormat:@"%@ : %d", _hostTextField.stringValue, i];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self scrollView:_resultScrollView addLine:str];
+                });
+            }
+        }
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            [self scrollView:_resultScrollView addLine:@"====== OVER ======"];
+        });
+    });
+
+
+
+}
+
+- (void)scrollView:(NSTextView *)textView addLine:(NSString *)string {
+    NSString *contentString = textView.string;
+    contentString = [contentString stringByAppendingString:string];
+    contentString = [contentString stringByAppendingString:@"\n"];
+    [textView setString:contentString];
+}
+
+- (BOOL)scanHost:(NSString *)host withPort:(int)port {
+    struct sockaddr_in server_addr;
+    server_addr.sin_len = sizeof(struct sockaddr_in);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr([host UTF8String]);
+    bzero(&(server_addr.sin_zero),8);
+
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // fcntl( server_socket, F_SETFL, O_NONBLOCK ); 
+    if (server_socket == -1) {
+        perror("socket error");
+        return NO;
+    }
+
+    if (connect(server_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in))==0)  {
+		printf("open %d\n", port);
+		close(server_socket);
+        return YES;
+    } else {
+		printf("not %d\n", port);
+        return NO;
+	}
+}
 @end
